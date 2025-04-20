bd34d900d4860d4bc61d730ceec0122190e96986
30xintegral <elvinmahmudzade04@gmail.com>
2025-04-21 00:09:59 +0400

    first commit

---
+++ src/main/java/com/demo/riskproject/RiskProjectApplication.java
@@ -0,0 +1,13 @@
+package com.demo.riskproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiskProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/riskproject/RiskProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.riskproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
