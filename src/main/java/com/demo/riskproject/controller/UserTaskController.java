d5d0bea2f270981b82c2429996545a56031914e4
30xintegral <elvinmahmudzade04@gmail.com>
2025-06-02 22:41:38 +0400

    some bugs are fixed

--- src/main/java/com/demo/riskproject/controller/UserTaskController.java
+++ src/main/java/com/demo/riskproject/controller/UserTaskController.java
@@ -7,6 +7,7 @@ import com.demo.riskproject.dto.response.UserTaskResponse;
 import com.demo.riskproject.service.UserTaskService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,6 +15,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/tasks")
+@Slf4j
 @CrossOrigin(origins = "https://intranet-banking.vercel.app/", allowCredentials = "true")
 public class UserTaskController {
     private final UserTaskService userTaskService;
@@ -21,15 +23,20 @@ public class UserTaskController {
 
     @GetMapping("/get-user-tasks")
     public PaginationResponse<UserTaskResponse> getUserTasks(@RequestParam(name = "isCompleted", required = false) Boolean isCompleted, @RequestParam(name = "page", defaultValue = "0") int page, @RequestParam(name = "size", defaultValue = "10") int size) {
+        log.info("get-user-tasks service called");
         return userTaskService.getUserTasks(isCompleted != null ? isCompleted : false, page, size);
     }
 
     @PatchMapping("/submit")
     public void submitTask(@ModelAttribute @Valid SingleUserTaskSubmission singleUserTaskSubmission) {
+        log.info("submit task service called");
         userTaskService.submitTask(singleUserTaskSubmission);
+        log.info("task submission completed");
     }
     @PostMapping("/assign")
     public void assignTaskToUsers(@RequestBody @Valid UserTaskRequest userTaskRequest) {
+        log.info("assign task service called");
         userTaskService.assignTaskToUsers(userTaskRequest);
+        log.info("task assignment completed");
     }
 }
